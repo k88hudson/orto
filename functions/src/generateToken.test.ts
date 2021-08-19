2703ba0512b434f498bab8dc211dfef527ccc1dd
Kate Hudson <k88hudson@gmail.com>
2021-08-19 10:11:37 -0400

    Add compat test

--- functions/src/generateToken.test.ts
+++ functions/src/generateToken.test.ts
@@ -1,6 +1,7 @@
 import { generateToken } from "./generateToken";
 import { generateKeyPairSync, verify } from "crypto";
-const STRUCTURE = [[0, 1], [1, 65], [65, 69], [69]];
+const CHROME_TOKEN =
+  "Aj+8ayHOoPdwyxMgiJdqXk6FhK50Vmc9bKwpmcr1gxUX4lsOJt21WKzhrKeaLzZwgymTXIIXN2FOX11mEwJtewEAAABleyJvcmlnaW4iOiJodHRwczovL2J1Z3p5Lm9yZzo0NDMiLCJmZWF0dXJlIjoiQ29tcHV0ZVByZXNzdXJlIiwiZXhwaXJ5IjoxNjM0MDgzMTk5LCJpc1N1YmRvbWFpbiI6dHJ1ZX0=";
 
 function getTestKeys(): { publicKey: string; privateKey: string } {
   const { publicKey, privateKey } = generateKeyPairSync("ed25519", {
@@ -12,7 +13,9 @@ function getTestKeys(): { publicKey: string; privateKey: string } {
 
 function getParts(token: string) {
   let buff = Buffer.from(token, "base64");
-  return STRUCTURE.map((indexes) => buff.slice(...indexes));
+  return [[0, 1], [1, 65], [65, 69], [69]].map((indexes) =>
+    buff.slice(...indexes)
+  );
 }
 
 test("verify token signature and parts", () => {
@@ -30,4 +33,17 @@ test("verify token signature and parts", () => {
   expect(length.readUInt32BE()).toBe(payload.byteLength);
   expect(parsedPayload).toEqual(testPayload);
   expect(verify(null, payload, publicKey, signature)).toBe(true);
+});
+
+test("tokens should be the same structure as Chrome", () => {
+  const [version, signature, length, payload] = getParts(CHROME_TOKEN);
+  const parsedPayload = JSON.parse(payload.toString());
+  const { publicKey } = getTestKeys();
+
+  expect([2, 3]).toContain(version.readInt8());
+  expect(length.readUInt32BE()).toBe(payload.byteLength);
+  expect(parsedPayload).toHaveProperty("origin");
+  expect(parsedPayload).toHaveProperty("expiry");
+  expect(parsedPayload).toHaveProperty("feature");
+  expect(verify(null, payload, publicKey, signature)).toBe(false);
 });
